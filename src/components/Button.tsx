fbda01443bde768e14a6dbbdd98601a772b0c2df
Pedro Henrique Salazar <ph.s.salazar@gmail.com>
2026-04-19 13:01:14 -0300

    fix: update button types and actions version

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,8 +1,12 @@
-import type React from "react"
+import type React from "react";
 
-function Button({ children, ...props }: { children: React.ReactNode }) {
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+    children: React.ReactNode;
+}
+
+function Button({ children, type = "button", ...props }: ButtonProps) {
     return (
-        <button {...props} className="bg-amber-500 px-10 py-2 rounded-full shadow shadow-slate-500 text-white hover:bg-amber-400 transition-colors cursor-pointer">
+        <button type={type} {...props} className="bg-amber-500 px-10 py-2 rounded-full shadow shadow-slate-500 text-white hover:bg-amber-400 transition-colors cursor-pointer">
             {children}
         </button>
     )
